cb12e36f663d2ac6f8948049292981446639658a
Juan Eugenio Abadie <juaneabadie@gmail.com>
2020-06-25 21:09:22 -0300

    Add example

---
+++ examples/from-stdin.rs
@@ -0,0 +1,19 @@
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    println!("Give me a tag-value FIX message, and I will give you a JSON");
+    println!("Do you need help? You can try this one: 8=FIX.4.2 | 10=20");
+
+    let stdin = io::stdin();
+    let handle = stdin.lock();
+
+    for line in handle.lines() {
+        if let Some(fix_message) = fixparser::FixMessage::from_tag_value(&line?) {
+            println!("{}", fix_message.to_json());
+        } else {
+            println!("Are your sure you gave me a valid FIX message?");
+        }
+    }
+
+    Ok(())
+}
